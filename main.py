1fdfb977ef4095dc53910e7cc21c188e1f3ddca9
ErinTomorri <97967950+ErinTomorri@users.noreply.github.com>
2022-06-30 17:02:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from pytube import YouTube
+# you may need to wait a few minutes for it to fully download
 
-"""
-def download_360p_mp4_videos(url: str, outpath: str = "./"):
+def download(url: str, outpath: str = "./"):
 
     yt = YouTube(url)
 
-    yt.streams.filter(file_extension="mp4").get_by_resolution("720p").download(outpath)
+    yt.streams.filter(file_extension="mp4").get_by_resolution("360p").download(outpath) #change the 360p to whatever quality you want it
 
 
 if __name__ == "__main__":
 
-    download_360p_mp4_videos(
-        "https://www.youtube.com/watch?v=ZkRjihsMdp8&ab_channel=pwnisher",
-        "./videos",
+    download(
+        "Link goes here", #this line is for your link
+        "./videos", # this is where the video will be placed, look for a folder called videos after the download is complete
     )
-"""
+
