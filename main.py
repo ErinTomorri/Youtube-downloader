6fcc64180a6fd0cff25fe6b254301774866fd089
ErinTomorri <97967950+ErinTomorri@users.noreply.github.com>
2022-06-30 16:59:43 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pytube import YouTube
+
+"""
+def download_360p_mp4_videos(url: str, outpath: str = "./"):
+
+    yt = YouTube(url)
+
+    yt.streams.filter(file_extension="mp4").get_by_resolution("720p").download(outpath)
+
+
+if __name__ == "__main__":
+
+    download_360p_mp4_videos(
+        "https://www.youtube.com/watch?v=ZkRjihsMdp8&ab_channel=pwnisher",
+        "./videos",
+    )
+"""
